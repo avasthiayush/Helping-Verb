d8d2535a2da5b4a9deccc158d196cb44b923cf10
Aman kumar singh <maskmanlucifer@gmail.com>
2020-06-28 12:46:03 +0530

    update 0.8 :boom:

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -4,24 +4,119 @@ let bodyParser = require('body-parser');
 /* Including mongoose for communicating to data base.*/
 let mongoose = require('mongoose');
 
+/*connect to database.*/
+mongoose.connect('mongodb://helpingverb:sonusir@helpingverb-shard-00-00-lonlm.mongodb.net:27017,helpingverb-shard-00-01-lonlm.mongodb.net:27017,helpingverb-shard-00-02-lonlm.mongodb.net:27017/helpingverb?ssl=true&replicaSet=HelpingVerb-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true,useUnifiedTopology: true })
+
+
+ /*model for contest.*/
+let Byschema=new mongoose.Schema({
+    name:String,
+    cname:{type:Array, default: ["","","","","","","","","",""]},
+    curl:{type:Array, default: ["","","","","","","","","",""]},
+    hname:{type:Array, default: ["","","","","","","","","",""]},
+    hurl:{type:Array, default: ["","","","","","","","","",""]},
+    oname:{type:Array, default: ["","","","","","","","","",""]},
+    ourl:{type:Array, default: ["","","","","","","","","",""]}
+ });
+
+ let noob={
+     name:"dummy",
+     arenaid:"1",
+     clink:"kkk",
+     skills:["","","","","","",""]
+ };
+ /*Model for profile.*/
+ let Myschema=new mongoose.Schema({
+    name:String,
+    arenaid:String,
+    clink:String,
+    skills:{type:Array, default: ["","","","","","",""]}
+ });
+ 
+ let First=mongoose.model('First',Myschema);
+
+ let Second=mongoose.model('Second',Myschema);
+
+ let Third=mongoose.model('Third',Myschema);
+
+ let Final=mongoose.model('Final',Myschema);
+
+ /* Creating model for blueprint.*/
+ let Contest=mongoose.model('Contest',Byschema);
+ 
 /*Firing urlpareser.*/
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /*Canavas.*/
 module.exports=function(app)
 {
+    app.get('/contest',function(req,res)
+    {
+        Contest.find({},function(err,data)
+        {
+            if(err)
+            {
+                process.exit(1);
+            }
+            res.render('contest',{data:data});
+        })
+    });
     app.get('/',function(req,res)
     {
         res.render('homepage');
+    });
+    app.get('/arena-1',function(req,res)
+    {
+        First.find({},function(err,data)
+        {
+            if(err)
+            {
+                process.exit(1);
+            }
+            res.render('arena-1',{data:data});
+        })
+    });
+    app.get('/arena-2',function(req,res)
+    {
+        Second.find({},function(err,data)
+        {
+            if(err)
+            {
+                process.exit(1);
+            }
+            res.render('arena-2',{data:data});
+        })
+    });
+    app.get('/arena-3',function(req,res)
+    {
+        Third.find({},function(err,data)
+        {
+            if(err)
+            {
+                process.exit(1);
+            }
+            res.render('arena-3',{data:data});
+        })
+    });
+    app.get('/arena-4',function(req,res)
+    {
+        Final.find({},function(err,data)
+        {
+            if(err)
+            {
+                process.exit(1);
+            }
+            res.render('arena-4',{data:data});
+        })
     });
     app.get('/profile',function(req,res)
     {
         res.render('profile');
     });
-    app.get('/contest',function(req,res)
-    {
-        res.render('contest'); 
-    });
+    // app.get('/contest',function(req,res)
+    // {
+    //     res.render('contest'); 
+    // });
     app.get('/avatar',function(req,res)
     {
         res.render('avatar');
